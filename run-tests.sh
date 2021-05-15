dd14540444eb52cbfff3c20ecda371152c0c19bc
wdlkmpx <wdlkmpx@gmail.com>
2021-05-15 21:32:23 +0800

    run-tests.sh: fix for FreeBSD
    FreeBSD:
       all scripts use /bin/sh
       md5sum is gmd5sum
    
    macOS: /bin/sh does understand echo -n
                   replace with printf

--- run-tests.sh
+++ run-tests.sh
@@ -1,8 +1,21 @@
-#!/bin/bash
+#!/bin/sh
+
 ALL_RET=0
-for SCRIPT in $(find $(dirname $0)/test/v* -name '*.sh'); do
-  echo -n "Running test $SCRIPT..."
-  bash ${SCRIPT}
+
+if command -v md5sum >/dev/null 2>&1 ; then
+    export MD5SUM=md5sum
+elif command -v gmd5sum >/dev/null 2>&1 ; then
+    export MD5SUM=gmd5sum
+else
+    echo "md5sum utility is missing, please install it"
+    echo "aborting tests"
+    exit 1
+fi
+
+for SCRIPT in $(find $(dirname $0)/test/v* -name '*.sh')
+do
+  printf "%s" "Running test $SCRIPT..."
+  sh ${SCRIPT}
   TEST_RET=$?
   if [ "$TEST_RET" = "0" ]; then
     echo "succeeded"
