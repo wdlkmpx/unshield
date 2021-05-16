e605b76134c5f620c1aeb2276671763af976e8d0
wdlkmpx <wdlkmpx@gmail.com>
2021-05-16 12:02:26 +0800

    .travis.yml: build for Windows mingw (gcc) and MSVC (VS 2017)

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
 set -e
 set -x
+
+if test "$TRAVIS_OS_NAME" = "windows" ; then
+    # see .travis.yml
+    if test "$TRAVIS_COMPILER" = "clang" ; then
+        # https://clang.llvm.org/docs/MSVCCompatibility.html
+        # clang on Windows uses MSVC stuff but it's not recognize but CMakeLists
+        # so must use MSVC instead, just unset some vars
+        echo "*** Will use MSVC ***"
+        export CFLAGS="-W3"
+        unset CC CC_FOR_BUILD CXX CXX_FOR_BUILD
+        .github/travis_windows_msvc.bat
+    else
+        export CFLAGS="-Wall" # gcc
+        .github/travis_windows_mingw.bat
+    fi
+    exit $?
+fi
+
 export CFLAGS="-Wall -Werror -ggdb3"
 cd `dirname $0`
 mkdir -p build
